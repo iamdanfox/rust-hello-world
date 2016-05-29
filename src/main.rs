550a03ce3da712aa658e8b838ff23a410ec37674
Dan Fox <iamdanfox@gmail.com>
2016-05-29 23:24:26 +0100

    Body parser extracts http body

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 extern crate iron;
+extern crate bodyparser;
 
 use iron::prelude::*;
 use iron::status;
 
+fn handler(req: &mut Request) -> IronResult<Response> {
+    println!("Received request to url {}", req.url);
+
+    let body = req.get::<bodyparser::Raw>();
+    match body {
+        Ok(Some(body)) => println!("Read body: '{}'", body),
+        Ok(None) => println!("No body"),
+        Err(err) => println!("Error: {:?}", err)
+    }
+
+    Ok(Response::with((status::Ok, "Hello World!")))
+}
+
 fn main() {
-    Iron::new(|_: &mut Request| {
-        Ok(Response::with((status::Ok, "Hello World!")))
-    }).http("localhost:3000").unwrap();
+    Iron::new(handler).http("localhost:3000").unwrap();
 }
 
 
