4b302af66817d69fc51f90b22308149960900aa2
Dan Fox <iamdanfox@gmail.com>
2016-05-29 23:58:42 +0100

    Serde deserialize body json {x:1, y:2}

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,4 @@
-extern crate iron;
-extern crate bodyparser;
+extern crate serde;
+extern crate serde_json;
 
-use iron::prelude::*;
-use iron::status;
-
-fn handler(req: &mut Request) -> IronResult<Response> {
-    println!("Received request to url {}", req.url);
-
-    let body = req.get::<bodyparser::Raw>();
-    match body {
-        Ok(Some(body)) => println!("Read body: '{}'", body),
-        Ok(None) => println!("No body"),
-        Err(err) => println!("Error: {:?}", err)
-    }
-
-    Ok(Response::with((status::Ok, "Hello World!")))
-}
-
-fn main() {
-    Iron::new(handler).http("localhost:3000").unwrap();
-}
-
-
-#[cfg(test)]
-mod test {
-    #[test]
-    fn green_test() {
-        assert!(true);
-    }
-
-    #[test]
-    #[should_panic]
-    fn should_panic() {
-        assert!(false);
-    }
-}
+include!(concat!(env!("OUT_DIR"), "/main.rs"));
